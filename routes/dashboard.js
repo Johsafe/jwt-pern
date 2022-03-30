8d8b2f8e31d5cb8315f1427fd44931e8b99e840e
Johsafe <78616295+Johsafe@users.noreply.github.com>
2022-03-30 08:56:50 +0300

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,10 +6,10 @@ const authorization =require("../middleware/authorization");
 router.get("/" ,authorization,async(req ,res)=>{
     try {
         //req.user has the payload
-         res.json(req.user);
+        // res.json(req.user);
 
-        // const user = await pool.query("SELECT user_name FROM users WHERE user_id =$1",[req.user])
-        // res.json(user.rows[0]);
+         const user = await pool.query("SELECT user_name FROM users WHERE user_id =$1",[req.user])
+         res.json(user.rows[0]);
         
     } catch (err) {
         console.error(err.message);
